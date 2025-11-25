2a34bcf8ee5b32cf303f04c5f6565fd35874dd4b
Jerwin Servito <jerwin.servito>
2025-11-26 01:43:44 +0800

    add env_prod frontend

--- frontend/src/helper/ApiConfig.js
+++ frontend/src/helper/ApiConfig.js
@@ -1,13 +1,36 @@
+// import axios from "axios";
+
+
+// const ApiRequest = axios.create({
+//     baseURL: '/api'
+// })
+
+// ApiRequest.interceptors.request.use(config => {
+//     config.headers['api-key'] = 'JerwinServitoSecretKey'
+//     return config
+// })
+
+// export default ApiRequest
+
 import axios from "axios";
 
+let baseURL = "";
+
+if (import.meta.env.DEV) {
+    // Local: use Vite dev server proxy
+    baseURL = "/api";
+} else {
+    // Production: use Render URL from .env
+    baseURL = import.meta.env.VITE_API_URL;
+}
 
 const ApiRequest = axios.create({
-    baseURL: '/api'
-})
+    baseURL,
+});
 
-ApiRequest.interceptors.request.use(config => {
-    config.headers['api-key'] = 'JerwinServitoSecretKey'
-    return config
-})
+ApiRequest.interceptors.request.use((config) => {
+    config.headers["api-key"] = "JerwinServitoSecretKey";
+    return config;
+});
 
-export default ApiRequest
+export default ApiRequest;
