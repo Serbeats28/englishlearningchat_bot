e64e6412a68f77e9492b4c7141b367eda015953a
Jerwin Servito <jerwin.servito>
2025-11-26 01:53:39 +0800

    fixing axios in frontend

--- frontend/src/helper/ApiConfig.js
+++ frontend/src/helper/ApiConfig.js
@@ -1,32 +1,9 @@
-// import axios from "axios";
-
-
-// const ApiRequest = axios.create({
-//     baseURL: '/api'
-// })
-
-// ApiRequest.interceptors.request.use(config => {
-//     config.headers['api-key'] = 'JerwinServitoSecretKey'
-//     return config
-// })
-
-// export default ApiRequest
-
 import axios from "axios";
 
-let baseURL = "";
-
-if (import.meta.env.DEV) {
-    // Local: use Vite dev server proxy
-    baseURL = "/api";
-} else {
-    // Production: use Render URL from .env
-    baseURL = import.meta.env.VITE_API_URL;
-}
-
 const ApiRequest = axios.create({
-    baseURL,
+    baseURL: process.env.VUE_APP_API_URL
 });
+
 
 ApiRequest.interceptors.request.use((config) => {
     config.headers["api-key"] = "JerwinServitoSecretKey";
